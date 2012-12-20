5157224ad3ff3b0ca5fc5aad23f99416401860ce
Andrei Fokau <andrei@5monkeys.se>
2012-12-20 13:27:51 +0100

    find packages

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 # coding=utf-8
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='rails',
       version='0.0a',
       download_url='git@github.com:andreif/django-rails.git',
-      packages=['rails'],
+      packages=find_packages(exclude=['_*']),
       author='Andrei Fokau',
       author_email='andrei@5monkeys.se',
       description='Ruby on Rails workflow for Django',
