9f4bff386b1558e9d63e61a7213ddda485fe87ad
Andrei Fokau <andrei@5monkeys.se>
2013-01-31 15:25:53 +0100

    add session wrapper, fix routing

--- rails/controllers.py
+++ rails/controllers.py
@@ -72,6 +72,41 @@ class Context(dict):
         return self.update(**kwargs)
 
 
+class Session(object):
+
+    def __init__(self, session):
+        self._session = session
+
+    def __getitem__(self, item):
+        return self._session.__getitem__(item)
+
+    def __setitem__(self, key, value):
+        return self._session.__setitem__(key, value)
+
+    def __setattr__(self, key, value):
+        if isinstance(key, basestring) and key.startswith('_'):
+            super(Session, self).__setattr__(key, value)
+        else:
+            self._session[key] = value
+
+    def __getattr__(self, item):
+        try:
+            if isinstance(item, basestring) and item.startswith('_'):
+                return self.__getattribute__(item)
+            else:
+                return self._session.__getattribute__(item)
+        except AttributeError as e:
+            if isinstance(item, basestring):
+                return self._session.get(item)
+            else:
+                raise e
+
+    def __call__(self, *args, **kwargs):
+        if args:
+            raise
+        return self._session.update(kwargs)
+
+
 class Request(object):
 
     def __init__(self, request):
@@ -124,7 +159,8 @@ class Router(object):
             self.controller_class = controller_class
         self.controller_name = self.controller_class._controller_name()
 
-    def get_routes(self):
+    def get_routes(self, prefix):
+        self.prefix = prefix
         cls = self.controller_class
         actions = cls._actions() + cls.simple_actions
         routes = [self.get_route(name) for name in actions]
@@ -140,12 +176,12 @@ class Router(object):
 
     def get_view(self, name, *args, **kwargs):
         def view(request, *args, **kwargs):
-            return self.controller_class(request, name)._render()
+            return self.controller_class(request, name)._render(args=args, kwargs=kwargs)
         return view
 
     def get_route(self, name, regex=None):
         return url(
-            regex or r'^/%s(\.[a-z]+)?/?$' % name.replace('_', '-'),
+            regex or r'^%s%s(\.[a-z]+)?/?$' % (self.prefix or '', name.replace('_', '-')),
             self.get_view(name),
             name='%s.%s' % (self.controller_name, name),
         )
@@ -199,15 +235,19 @@ class BaseController(View):
         super(BaseController, self).__init__()
         self.cx = Context()
         self.rq = Request(request)
+        self.ss = Session(request.session)
         self.router = Router(self.__class__)
         self.renderer = Renderer(self)
         self._current_action = action
         self.cx.current_action = action
+        self.route_args = self.route_kwargs = None
 
     def _redirect(self, to='/', params=None, obj=None):
         if to == ':back':
             url = self.rq.request.META['HTTP_REFERER']
         else:
+            if to.startswith('#'):
+                to = self._controller_name() + '.' + to[1:]
             url = self.router.reverse(to)
             if obj:
                 params = params or {}
@@ -258,13 +298,20 @@ class BaseController(View):
     def _before_render(self):
         pass
 
-    def _render(self, name=None):
+    def _render(self, name=None, args=None, kwargs=None):
+        if name and name.startswith('#'):
+            name = name[1:]
+
+        self.route_args = self.route_args or args
+        self.route_kwargs = self.route_kwargs or kwargs
+
         name = name or self._current_action
         self.cx(
             current_action=self._current_action,
             current_controller=self._controller_name(),
             title='%s | %s' % (self._controller_name(), name)
         )
+        print name, self._actions()
         if name in self._actions():
             self._setup_render()
             response = self._before_render() or getattr(self, name)()
@@ -296,8 +343,8 @@ class BaseController(View):
         return [name for name in dir(cls) if cls._is_action(name)]
 
     @classmethod
-    def routes(cls):
-        return Router(cls).get_routes()
+    def routes(cls, prefix='/'):
+        return Router(cls).get_routes(prefix)
 
     @classmethod
     def render_view(cls, name, request, *args, **kwargs):
